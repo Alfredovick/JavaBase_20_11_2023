7faf004e571993744e5137701b5bede8aeed3417
Zirukin Roman <foxroman2oh@gmail.com>
2023-12-07 13:44:50 +0200

    add all files

--- src/Main/java/ua/hillel/zirukin/lessons/lesson4/homeWork5.java
+++ src/Main/java/ua/hillel/zirukin/lessons/lesson4/homeWork5.java
@@ -4,6 +4,27 @@ public class homeWork5 {
 
     public void homeWork5(String[] args) {
 
+        int unitsWarriorLi = 860;
+        int unitArcherLi = 860;
+        int unitHorsemenLI = 860;
+
+        int unitsWarriorsMin = (int) (1.5 * unitsWarriorLi);
+        int unitsArchersMin = (int) (1.5 * unitArcherLi);
+        int unitsCavalryMin = (int) (1.5 * unitHorsemenLI);
+
+        int attackWarriorLi = 13;
+        int attackArcherLi = 24;
+        int attackCavalryLi = 46;
+
+        int attackWarriorMin = 9;
+        int attackArcherMin = 35;
+        int attackCavalryMin = 12;
+
+        int totalAttackMin = unitsWarriorsMin * attackWarriorMin + unitsArchersMin * attackArcherMin + unitsCavalryMin * attackCavalryMin;
+        int totalAttackLi = unitsWarriorLi * attackWarriorLi + unitArcherLi * attackArcherLi + unitHorsemenLI * attackCavalryLi;
+
+        System.out.println("totalAttackLi" + totalAttackLi);
+        System.out.println("totalAttackMin" + totalAttackMin);
 
     }
 }
