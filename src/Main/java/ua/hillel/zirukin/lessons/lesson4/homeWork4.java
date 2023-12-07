1be6ba72978a199db4c42fc1c60b9073503eaf0c
Zirukin Roman <foxroman2oh@gmail.com>
2023-12-07 12:57:51 +0200

    add all files

--- src/Main/java/ua/hillel/zirukin/lessons/lesson4/homeWork4.java
+++ src/Main/java/ua/hillel/zirukin/lessons/lesson4/homeWork4.java
@@ -4,6 +4,20 @@ public class homeWork4 {
 
     public void homeWork4(String[] args) {
 
+        double a = 5;
+        double b = 6;
+        double c = 8;
+
+        double all = a * b * c;
+
+        System.out.println(" all =" + all);
+
+        double length = a + b + c;
+
+        System.out.println(" length =" + length);
+
+
+
     }
 
 }
