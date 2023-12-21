5fe6aaa993bb878e07d00351563560a2e559cc9b
Zirukin Roman <foxroman2oh@gmail.com>
2023-12-21 18:55:45 +0200

    add all files

---
+++ src/Main/java/ua/hillel/zirukin/lessons/lesson7/lesson7.java
@@ -0,0 +1,74 @@
+package ua.hillel.zirukin.lessons.lesson7;
+
+public class lesson7 {
+    public static void main(String[] args) {
+
+//        int [] array = new int [5];
+
+//        array[0] = 10;
+//        array[1] = 2;
+//        array[2] = 3;
+//        array[3] = 4;
+//        array[4] = 5;
+//
+//        System.out.println(array[0]);
+//        System.out.println(array[1]);
+//        System.out.println(array[2]);
+//        System.out.println(array[3]);
+//        System.out.println(array[4]);
+
+//        int[] array = new int [10];
+
+//        for (int i = 0; i < 10; i++) {
+//            array[i] = i + 1;
+//        }
+//
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(array[i]);
+//        }
+
+//        int[] array = new int [10];
+
+//        array[10] = 1; ArrayIndexOutOfBoundsException
+
+//        int[] array = new int[10];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = (int) (Math.random() * 11);
+//        }
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (i == array.length - 1) {
+//                System.out.println(array[i]);
+//            } else {
+//                System.out.print(array[i] + ", ");
+//            }
+//
+//        }
+
+//        int[] people = {11, 44, 98, 44, 22, 33, 55, 98, 6};
+//
+//        int maxValue = people[0];
+//        int indexMaxValue = 0;
+//
+//        for (int i = 0; i< people.length; i++) {
+//            if (people[i] > maxValue) {
+//                maxValue = people[i];
+//                indexMaxValue = i;
+//            }
+//        }
+//
+//        System.out.println("max value = " + maxValue + ", indext: " + indexMaxValue);
+
+//        int[] team = new int [10];
+//
+//        for (int i = 0; i < team.length; i++); {
+//            team[i] = (int) (Math.random() * 11);
+//        }
+//        System.out.println(Arrays.toString(team));
+//
+//        int teamOne = 0;
+//        int teamTwo = 0;
+
+    }
+}
